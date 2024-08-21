6f8cff246bd4d7b5789e042d5f18595e0697ccf7
pgudi <prabhakargudi@gmail.com>
2024-08-21 12:55:32 +0530

    Collection framework day 1

---
+++ SampleJavaPrograms/src/com/sgtesting/util/UnBoxingDemo.java
@@ -0,0 +1,16 @@
+package com.sgtesting.util;
+
+public class UnBoxingDemo {
+    public static void main(String[] args) {
+        Integer a=130;
+        System.out.println("Integer a :"+a);
+
+        //Convert into primitive value
+        int b=a.intValue();
+        System.out.println("int b:"+b);
+
+        //Unboxing
+        int c=a;
+        System.out.println("int c :"+c);
+    }
+}
